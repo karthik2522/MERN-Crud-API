e96e994ffb5174a46b6c0ab6106acf5b02de8455
Karthik Prakash Hudedamani <61647777+karthik2522@users.noreply.github.com>
2024-05-01 08:41:25 -0400

    Update product.controller.js
    .

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -8,7 +8,7 @@ const getProducts = async (req,res) => {
         const products = await Product.find({});
         res.status(200).json(products);
     }catch(error){
-        res.status(500).json({message: error.message})
+        res.status(500).json({message: error.message});
     }
 }
 
