607cf2a2449838fdb6c271f895fa639bbf2053f1
Karthik Prakash Hudedamani <61647777+karthik2522@users.noreply.github.com>
2024-05-05 07:49:27 -0400

    Update product.route.js
    imported route function

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,6 +1,17 @@
 const express = require("express");
-const Product = require("./models/product.model.js");
 const router = express.Router();
-const {getProducts}=require('../controller/product.controller.js');
+const { getProducts, getProduct, createProduct, updateProduct, deleteProduct } = require('../controller/product.controller.js');
 
-router.get('/', getProducts );
+router.get('/', getProducts);
+router.get('/:id', getProduct);
+
+router.post('/', createProduct);
+
+//update Product
+
+router.put('/:id', updateProduct);
+
+//delete Product
+router.delete('/:id', deleteProduct);
+
+module.exports = router;
