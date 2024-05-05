2dbf754fcc2488efb67e008186c488db1c9a16f9
Karthik Prakash Hudedamani <61647777+karthik2522@users.noreply.github.com>
2024-05-05 07:54:14 -0400

    Update product.route.js
    added comments

--- routes/product.route.js
+++ routes/product.route.js
@@ -2,13 +2,14 @@ const express = require("express");
 const router = express.Router();
 const { getProducts, getProduct, createProduct, updateProduct, deleteProduct } = require('../controller/product.controller.js');
 
+// Get Products
 router.get('/', getProducts);
 router.get('/:id', getProduct);
 
+//create Product
 router.post('/', createProduct);
 
 //update Product
-
 router.put('/:id', updateProduct);
 
 //delete Product
